ae31c58bc5daa03c83c3482ac17d78dcd664db64
Alexander Kolev <alexander.kolev@gmail.com>
2017-05-19 23:28:03 +0300

    word2vec sample

---
+++ samples/test_word2vec.py
@@ -0,0 +1,17 @@
+import os
+import pdb
+import gensim
+
+SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
+
+
+# load word2vec model
+model = gensim.models.Word2Vec.load(os.path.join(SCRIPT_PATH, 'bgwiki_word2vec'), mmap='r')
+
+
+# find most similar words
+for word, sim in model.most_similar(positive=[u'роднина'], topn=5):
+    print('\"%s\"\t- similarity: %g' % (word, sim))
+
+# get coordinates for a particular word
+print(model.wv[u'роднина'])
